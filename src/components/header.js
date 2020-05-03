321423843ededa5146a09ee40f11b09288866193
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-05-03 18:45:23 -0400

    began the skeleton on the header as well as the navigation bar

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import logo from '../assets/Hop-N-Cru-Logo.jpg'
+
+
+class Header extends Component {
+  render()  {
+    return(
+      <React.Fragment>
+        <div className='header-main'>
+          <div className='logo'>
+            <img src={logo} alt='Hop-N-Cru Logo' id='logo-image'></img>
+          </div>
+        </div>
+        <div id='header-nav'>
+          <ul id='nav-items'>
+            <li className='nav-list-item'>HOME</li>
+            <li className='nav-list-item'>ABOUT</li>
+            <li className='nav-list-item'>STORE INFO</li>
+          </ul>
+        </div>
+      </ React.Fragment>
+    )
+  }
+}
+
+
+export default Header;
