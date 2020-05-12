d843a43e8026641ad2fd777e29054bb87195d4e2
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-05-11 20:42:54 -0400

    cleaned up the home page style and added some links to google maps

--- src/components/home.js
+++ src/components/home.js
@@ -15,14 +15,19 @@ class Home extends Component {
                (240) 801-8550
             </h3>
           </div>
+          <div className='home-images'>
+            <img src='https://i.imgur.com/Uhi43m4.jpg' id='delivery'></img>
+          </div>
           <div className='about-info-contain'>
-            <h2>About Us</h2>
-            <h4>We have the largest selection of craft beer and fine wines in Germantown, MD. <br/>
-            We do special orders and offer discounts on large orders too.<br/> 
-            Located in the Neelsville Village Center, next door to Giant Foods.</h4>
+            <h2>About us:</h2>
+            <h4>- We have the largest selection of craft beer and fine wines in Germantown, MD. </h4>
+            <h4>- We do special orders and offer discounts on large orders too.</h4>
+            <h4>- Located in the Neelsville Village Center, next door to Giant Foods.</h4>
           </div>
           <div className='map-image-div'>
-              <img src={map} alt='hop-map' id='map-image'></img>
+              <a
+              id='image-link'
+              href='https://www.google.com/maps?q=hop+n+cru&rlz=1C5CHFA_enUS881US881&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjr3bPlg63pAhX5kHIEHbsXBRgQ_AUoA3oECBcQBQ'><img src={map} alt='hop-map' id='map-image'></img></a>
             <div className='image-click-div'>
               <h3>Click to open in google maps</h3>
             </div>
--- src/components/item.js
+++ src/components/item.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 
 class Item extends Component {
-  constructor(props)  {
-    super(props);
-  }
+  // constructor(props)  {
+  //   super(props);
+  // }
 
   render()  {
     return(
       <React.Fragment>
         <div className='item-contain'>
-        <img src={this.props.data.image} className='item-image' alt='beer-picture'></img>
+        <img src={this.props.data.image} className='item-image' alt='beer'></img>
           <div className='item-info-contain'>
             <h2>{this.props.data.name}</h2>
             <h4>${this.props.data.price}.00</h4>
